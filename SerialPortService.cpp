a60d68657189d4b89460c43cf9bf62caad0b7989
ENVISION\xiaowei.han <xiaowei_han@yeah.net>
2017-05-22 19:37:49 +0800

    贡献Asio操作COM的类。

---
+++ SerialPortService.cpp
@@ -0,0 +1,296 @@
+#include "stdafx.h"
+#include "SerialPortService.h"
+#include <boost/bind.hpp>
+#include <boost/asio/placeholders.hpp>
+#include <boost/thread.hpp>
+#include <boost/date_time/posix_time/ptime.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <iostream>
+//接收缓冲区的大小(1MB)
+const int MAX_BUFFER_SIZE = (1 << 20);
+
+CSerialPortService::CSerialPortService():
+	m_Work(m_IoService),
+	m_SerialPort(m_IoService),
+	m_nUseBufferBytes(0),
+	m_pRecvBuffer(nullptr)
+{
+
+	if (nullptr == m_pRecvBuffer)
+	{
+		m_pRecvBuffer = new BYTE[MAX_BUFFER_SIZE];
+	}
+
+}
+
+
+CSerialPortService::~CSerialPortService()
+{
+	if (nullptr != m_pRecvBuffer)
+	{
+		delete[]m_pRecvBuffer;
+		m_pRecvBuffer = nullptr;
+	}
+	m_nUseBufferBytes = 0;
+}
+
+void CSerialPortService::StartSerialPortService(void)
+{
+	//初始化串口
+	if (!InitSerialPort())
+	{
+		return;
+	}
+	//开始异步读取数据
+	m_SerialPort.async_read_some(boost::asio::buffer(m_pRecvBuffer + m_nUseBufferBytes, MAX_BUFFER_SIZE - m_nUseBufferBytes), boost::bind(&CSerialPortService::ReadHandler, this, boost::asio::placeholders::error,
+		                                              boost::asio::placeholders::bytes_transferred));
+	
+	boost::system::error_code ec;
+	m_IoService.run(ec);
+}
+
+void CSerialPortService::StopSerialPortService(void)
+{
+	m_IoService.stop();
+}
+
+
+void CSerialPortService::AsyncWriteData(BYTE* pData, int nDataLength)
+{
+	if (nullptr == pData || 0 == nDataLength)
+	{
+		return;
+	}
+
+	boost::this_thread::sleep(boost::posix_time::seconds(1));
+	m_SerialPort.async_write_some(boost::asio::buffer(pData,nDataLength),boost::bind(&CSerialPortService::WriteHandler,this,pData,nDataLength, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+}
+
+//任务通知完成回调
+void CSerialPortService::ReadHandler(const boost::system::error_code& ec, std::size_t bytes_transferred)
+{
+	if (ec)
+	{
+		return;
+	}
+	//分析是否有整的数据包
+	int nRecvBytes = static_cast<int>(bytes_transferred);
+	m_nUseBufferBytes += nRecvBytes;
+	//处理数据
+	int nHandledBytes = HandlerData(m_pRecvBuffer,m_nUseBufferBytes);
+	//移动数据块
+	memmove(m_pRecvBuffer, m_pRecvBuffer + nHandledBytes, (m_nUseBufferBytes - nHandledBytes));
+	m_nUseBufferBytes -= nHandledBytes;
+	m_SerialPort.async_read_some(boost::asio::buffer(m_pRecvBuffer + m_nUseBufferBytes, MAX_BUFFER_SIZE - m_nUseBufferBytes), boost::bind(&CSerialPortService::ReadHandler, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+}
+
+/**************************************************************************
+@FunctionName:HandlerData
+@FunctionDestription:处理数据
+@InputParam:
+@OutPutParam:
+@ReturnValue:返回已经处理的字节数
+**************************************************************************/
+int CSerialPortService::HandlerData(BYTE* pData, int nDataLength)
+{
+	int nHandledBytes = 0;
+	if (nullptr == pData || 0 == nDataLength)
+	{
+		return 0;
+	}
+	char szSensorName[100] = { 0 };
+	int nIndex = 0;
+
+	while (nIndex < nDataLength)
+	{	
+		//传感器上下线通知
+		if (pData[nIndex] == 0xBA)
+		{
+			//确保数组不越界
+			if (nIndex + 1 < nDataLength)
+			{
+				//获取数据长度
+				int nFrameLength = (int)pData[nIndex + 1];
+				//判断整包长度
+				if ((nIndex + nFrameLength + 1) < nDataLength)
+				{
+					if (CalCRC8(&pData[nIndex], nFrameLength + 1) == pData[nIndex + nFrameLength + 1])
+					{
+						int nSensorNameLength = (int)pData[nIndex + 1 + 1];
+						//获取传感器名称
+						memcpy(szSensorName, &pData[nIndex + 1 + 1 + 1], nSensorNameLength);
+						if (pData[nIndex + 1 + 1 + 1 + nSensorNameLength] == 0x01)
+						{
+							std::cout << szSensorName << " is on! " << "thread ID = ["<<boost::this_thread::get_id()<<"]"<<std::endl;
+						}
+						else
+						{
+							std::cout << szSensorName << " is off! " << "thread ID = [" << boost::this_thread::get_id() << "]" << std::endl;
+						}
+						nIndex += (nFrameLength + 2);
+						//修改数据的处理
+						nHandledBytes += (nFrameLength + 2);
+						continue;
+					}
+				}
+			}
+		}
+
+		//传感器上报数据通知
+		else if (pData[nIndex] == 0xBD)
+		{
+			//处理数据上报通知
+			if (nIndex + 1 < nDataLength)
+			{
+				//获取数据长度
+				int nFrameLength = (int)pData[nIndex + 1];
+				//判断整包长度
+				if ((nIndex + nFrameLength + 1) < nDataLength)
+				{
+					if (CalCRC8(&pData[nIndex], nFrameLength + 1) == pData[nIndex + nFrameLength + 1])
+					{
+						int nSensorNameLength = (int)pData[nIndex + 1 + 1];
+						//获取传感器名称
+						memcpy(szSensorName, &pData[nIndex + 1 + 1 + 1], nSensorNameLength);
+						float fValue = 0.0f;
+						memcpy(&fValue, &pData[nIndex + 1 + 1 + 1 + nSensorNameLength], 4);
+						nIndex += (nFrameLength + 2);
+						//修改数据的处理
+						nHandledBytes += (nFrameLength + 2);
+						boost::posix_time::ptime p = boost::posix_time::second_clock::local_time();
+						std::cout << "["<<p<<"] "<<"the sensor [" << szSensorName << "] value is [" << fValue << "]" << std::endl;
+						continue;
+					}
+				}
+			}
+		}
+		else
+		{		
+		}
+		++nIndex;
+	}
+	return nHandledBytes;
+}
+
+
+BYTE CSerialPortService::CalCRC8(BYTE* pBuf, unsigned int nsize)
+{
+	BYTE crc = 0;
+
+	if (nullptr == pBuf || 0 == nsize)
+	{
+		return crc;
+	}
+
+	while (nsize--)
+	{
+		crc ^= *pBuf;
+		pBuf++;
+	}
+	return crc;
+}
+
+void CSerialPortService::WriteHandler(BYTE* pData, int nDataLength, const boost::system::error_code& ec, std::size_t bytes_transferred)
+{
+	using namespace std;
+	if (ec)
+	{
+		return;
+	}
+
+	if (nDataLength != bytes_transferred)
+	{
+		cout << "write serial failed!" << endl;
+		return;
+	}
+
+	if (nullptr != pData)
+	{
+		delete[] pData;
+		pData = nullptr;
+	}
+}
+
+bool CSerialPortService::InitSerialPort(void)
+{
+
+#if 0
+	boost::system::error_code ec;
+	m_SerialPort.open(m_SerialOption.strSerialPort, ec);
+	if (ec)
+	{
+		return false;
+	}
+	//波特率
+	m_SerialPort.set_option(boost::asio::serial_port::baud_rate(m_SerialOption.nBaudRate),ec);    
+	if (ec)
+	{
+		return false;
+	}
+
+	if (m_SerialOption.bFlowControl)
+	{
+		m_SerialPort.set_option(boost::asio::serial_port::flow_control(boost::asio::serial_port::flow_control::hardware));  //流控制
+	}
+	else
+	{
+		m_SerialPort.set_option(boost::asio::serial_port::flow_control(boost::asio::serial_port::flow_control::none));  //流控制
+	}
+	if (ec)
+	{
+		return false;
+	}
+
+	switch (m_SerialOption.nPairty)
+	{
+		//不使用校验
+	case 0:
+		m_SerialPort.set_option(boost::asio::serial_port::parity(boost::asio::serial_port::parity::none),ec); //奇偶校验
+		break;
+		//奇数校验
+	case 1:
+		m_SerialPort.set_option(boost::asio::serial_port::parity(boost::asio::serial_port::parity::odd), ec);
+		break;
+		//偶数校验
+	case 2:
+		m_SerialPort.set_option(boost::asio::serial_port::parity(boost::asio::serial_port::parity::even), ec);
+		break;
+	default:
+		m_SerialPort.set_option(boost::asio::serial_port::parity(boost::asio::serial_port::parity::none), ec);
+		break;
+	}
+	if (ec)
+	{
+		return false;
+	}
+
+	switch (m_SerialOption.nStopBits)
+	{
+	case 0:
+		m_SerialPort.set_option(boost::asio::serial_port::stop_bits(boost::asio::serial_port::stop_bits::one),ec); //停止位
+		break;
+	case 1:
+		m_SerialPort.set_option(boost::asio::serial_port::stop_bits(boost::asio::serial_port::stop_bits::onepointfive),ec); //停止位
+		break;
+	case 2:
+		m_SerialPort.set_option(boost::asio::serial_port::stop_bits(boost::asio::serial_port::stop_bits::two),ec); //停止位
+		break;
+	default:
+		m_SerialPort.set_option(boost::asio::serial_port::stop_bits(boost::asio::serial_port::stop_bits::one), ec);
+		break;
+	}
+
+	if (ec)
+	{
+		return false;
+	}
+
+	m_SerialPort.set_option(boost::asio::serial_port::character_size(m_SerialOption.nDataBits),ec);// 通信字节位数，4—8
+	if (ec)
+	{
+		return false;
+	}
+#endif
+	return true;
+}
+
---
+++ SerialPortService.h
@@ -0,0 +1,77 @@
+#pragma once
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/serial_port.hpp>
+#include <string>
+typedef struct _comproperty
+{
+	//串口编号
+	std::string strSerialPort;
+	//波特率
+	unsigned int nBaudRate;
+	//通信字节位数
+	int nDataBits;
+	//(0:1个停止位 1:1.5个停止位(Linux暂不支持1.5个停止位) 2:2个停止位)
+	int nStopBits;
+	//(0:不使用校验  1:奇数校验 2:偶数校验 3:标记校验（Linux下没有此项） 4:空格校验)
+	int nPairty;
+	//(0:不使用流控 1:使用硬件流控)
+	bool bFlowControl;
+	_comproperty()
+	{
+		strSerialPort = "";
+		nBaudRate = 9600;
+		nDataBits = 8;
+		nStopBits = 0;
+		nPairty = 0;
+		bFlowControl = false;
+	}
+}COMPROPERTY, *LPCOMPROPERTY;
+
+class CSerialPortService
+{
+public:
+	CSerialPortService();
+	~CSerialPortService();
+
+
+	void SetSerialPortOption(const COMPROPERTY& Options)
+	{
+		m_SerialOption = Options;
+	}
+
+	//开启服务
+	void StartSerialPortService(void);
+
+	//停止服务
+	void StopSerialPortService(void);
+
+	//设置传感器开始采集
+	void AsyncWriteData(BYTE* pData, int nDataLength);
+private:
+	//接收完成通知
+	void ReadHandler(const boost::system::error_code& ec, std::size_t bytes_transferred);
+
+	//判断是否有整包数据
+	int HandlerData(BYTE* pData,int nDataLength);
+
+	//进行CRC校验和
+	BYTE CalCRC8(BYTE* pBuf, unsigned int nsize);
+
+	//发送完成通知
+	void WriteHandler(BYTE* pData, int nDataLength, const boost::system::error_code& ec, std::size_t bytes_transferred);
+
+private:
+	//初始化串口
+	bool InitSerialPort(void);
+	//接收缓冲区
+	BYTE* m_pRecvBuffer;
+	//已经占用的字节
+	int m_nUseBufferBytes;
+	//串口通信配置项
+	COMPROPERTY m_SerialOption;
+	//IoService
+	boost::asio::io_service m_IoService;
+	boost::asio::io_service::work m_Work;
+	boost::asio::serial_port m_SerialPort;
+};
+
