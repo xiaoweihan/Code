dd8a714347059b1a974fa9700298241ec64d5f05
xiaowei_han <xiaowei_han@yeah.net>
2017-07-13 22:52:49 +0800

    实现部分clientsocket的代码。

--- AsioSocket/Client/AsioSocket.cpp
+++ AsioSocket/Client/AsioSocket.cpp
@@ -1,3 +1,66 @@
 
+/**************************************************************************
+Copyright(C):
+FileName:AsioSocket.cpp
+Description:基于Asio机制的Socket
+Author:xiaowei.han
+Date:2017/07/13
+Others:
+Histroy:
+**************************************************************************/
 #include "stdafx.h"
 #include "AsioSocket.h"
+
+CAsioSocketClient::CAsioSocketClient():
+	m_Work(m_IOService),
+	m_AsioSocket(m_IOService)
+{
+
+}
+
+/*******************************************************************
+*函数名称:AsyncConnect
+*功能描述:同步连接服务器
+*输入参数:
+*输出参数:
+*返回值:
+*作者:xiaowei.han
+*日期:2017/07/13 22:51:32
+*******************************************************************/
+bool CAsioSocketClient::AsyncConnect(const std::string strAddress, unsigned short usPort)
+{
+	if (strAddress.empty())
+	{
+		return false;
+	}
+
+	//构建Remot Endpoint
+	auto RemoteAddress = boost::asio::ip::address::from_string(strAddress);
+	boost::asio::ip::tcp::endpoint RemotePoint(RemoteAddress,usPort);
+	boost::system::error_code ec;
+	m_AsioSocket.connect(RemotePoint, ec);
+
+	if (ec)
+	{
+		return false;
+	}
+
+	return true;
+
+}
+
+/*******************************************************************
+*函数名称:AsyncConnect
+*功能描述:同步连接服务器
+*输入参数:
+*输出参数:
+*返回值:
+*作者:xiaowei.han
+*日期:2017/07/13 22:51:32
+*******************************************************************/
+bool CAsioSocketClient::AsyncConnect(const char* szAddress, unsigned short usPort)
+{
+
+	return AsyncConnect(std::string(szAddress), usPort);
+
+}
--- AsioSocket/Client/AsioSocket.h
+++ AsioSocket/Client/AsioSocket.h
@@ -14,15 +14,25 @@ Histroy:
 #include <boost/asio.hpp>
 #include <boost/asio/io_service.hpp>
 #include <boost/asio/ip/address.hpp>
-
+#include <string>
 //基于practor模式的套接字
-class CAsioSocket
+class CAsioSocketClient
 {
 public:
-	CAsioSocket() = default;
-	~CAsioSocket() = default;
+	CAsioSocketClient();
+	~CAsioSocketClient() = default;
 
-
+public:
+	//同步连接
+	bool AsyncConnect(const std::string strAddress, unsigned short usPort);
+	bool AsyncConnect(const char* szAddress, unsigned short usPort);
+private:
+	//IOService
+	boost::asio::io_service m_IOService;
+	//work 用于循环不退出
+	boost::asio::io_service::work m_Work;
+	//套接字
+	boost::asio::ip::tcp::socket m_AsioSocket;
 };
 
 
